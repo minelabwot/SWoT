2774f8c4db5f457e688633df0628f5f764c646e4
Yang Yunong <csyyn33@sina.com>
2017-09-19 11:16:37 +0800

    change owl

--- Message_Passing_Algorithm_11_14/edu/bupt/semantic/Relation.java
+++
@@ -1,9 +0,0 @@
-package bupt.semantic;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class Relation {
-	Set<String> existRel = new HashSet<String>();  //用于offline版本
-	String currentValues = null;
-}
--- Message_Passing_Algorithm_11_14/edu/bupt/semantic/Type.java
+++
@@ -1,9 +0,0 @@
-package bupt.semantic;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class Type {
-	Set<String> Candidate = new HashSet<String>();
-	String currentValues = null;
-}
